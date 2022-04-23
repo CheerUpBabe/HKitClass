04a271e064ca435102daa6d299d013ee15020e0d
CheerUpBabe <ojoj816@gmail.com>
2022-04-23 22:40:57 +0900

    first commit

---
+++ test.java
@@ -0,0 +1,13 @@
+public class Alphabet{
+	public static void main(String[] args){
+		int num1=1;
+		
+		while (num1<30){
+			if (num1%2==0){
+				System.out.print(num1+" ");
+			}
+			num1++;
+		}
+		
+		System.out.println();
+}
